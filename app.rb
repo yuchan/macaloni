ead1640b1b9422bc2fe8c9b01520e9f8b0bdfb80
Yusuke Ohashi <github@junkpiano.me>
2017-01-08 21:18:42 +0900

    caches token in local file.

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'twitter'
 require 'oauth'
 require 'thor'
 require 'launchy'
+require 'json'
 
 class Macaloni < Thor
   desc "follow_followers", "follow your followers who tweets within the last 3 months."
@@ -38,32 +39,42 @@ class Macaloni < Thor
   private
 
   def twitter
-    key        = ENV["TWITTER_CONSUMER_KEY"]
-    secret     = ENV["TWITTER_CONSUMER_SECRET"]
+    content = {}
+    begin
+      jsonstring = File.read("/tmp/macaloni.json")
+      content = JSON.parse(jsonstring)
+    rescue
+      key        = ENV["TWITTER_CONSUMER_KEY"]
+      secret     = ENV["TWITTER_CONSUMER_SECRET"]
 
-    consumer = OAuth::Consumer.new(key, secret, site: Twitter::REST::Client::BASE_URL)      
-    request_token = consumer.get_request_token
+      consumer = OAuth::Consumer.new(key, secret, site: Twitter::REST::Client::BASE_URL)      
+      request_token = consumer.get_request_token
 
-    request = consumer.create_signed_request(:get, consumer.authorize_path, request_token, {oauth_callback: 'oob'})
-    params = request['Authorization'].sub(/^OAuth\s+/, '').split(/,\s+/).collect do |param|
-      key, value = param.split('=')
-      value =~ /"(.*?)"/
-      "#{key}=#{CGI.escape(Regexp.last_match[1])}"
-    end.join('&')
-    uri = "#{Twitter::REST::Client::BASE_URL}#{request.path}?#{params}"
-    Launchy.open(uri)
-    puts 'Enter the PIN: '
-    pin = $stdin.gets.chomp
-
-    access_token = request_token.get_access_token(oauth_verifier: pin)
-
-    cli = Twitter::REST::Client.new do |config|
-      config.consumer_key        = access_token.consumer.key
-      config.consumer_secret     = access_token.consumer.secret
-      config.access_token        = access_token.token
-      config.access_token_secret = access_token.secret
+      request = consumer.create_signed_request(:get, consumer.authorize_path, request_token, {oauth_callback: 'oob'})
+      params = request['Authorization'].sub(/^OAuth\s+/, '').split(/,\s+/).collect do |param|
+        key, value = param.split('=')
+        value =~ /"(.*?)"/
+        "#{key}=#{CGI.escape(Regexp.last_match[1])}"
+      end.join('&')
+      uri = "#{Twitter::REST::Client::BASE_URL}#{request.path}?#{params}"
+      Launchy.open(uri)
+      puts 'Enter the PIN: '
+      pin = $stdin.gets.chomp
+      begin
+        access_token = request_token.get_access_token(oauth_verifier: pin)
+        config = {
+          consumer_key:         access_token.consumer.key,
+          consumer_secret:      access_token.consumer.secret,
+          access_token:         access_token.token,
+          access_token_secret:  access_token.secret      
+        } 
+        content = config.to_json
+        File.write("/tmp/macaloni.json", configjson)
+      rescue
+        raise
+      end
     end
-    cli
+    Twitter::REST::Client.new(content)
   end
 
   def active(cli, user)
