2f51b98538a3cb100fc9703b09e4ca4a51321628
Yusuke Ohashi <github@junkpiano.me>
2017-08-18 12:53:38 +0900

    add new features.

--- lib/macaloni/cli.rb
+++ lib/macaloni/cli.rb
@@ -8,17 +8,12 @@ module Macaloni
 
     desc "follow_followers", "follow your followers who tweets within the last 3 months."
     def follow_followers
-      t.followers.each do |follower|
-        user = t.user(follower.id)
-        next if user.following?
-        begin
-          t.follow(follower.id) if active(user)
-        rescue Twitter::Error::TooManyRequests
-          raise
-        rescue Twitter::Error::Unauthorized
-          p 'this account might be private.'
-        end
-      end
+      _follow_followers(false)
+    end
+
+    desc "follow_followers_with_mute", "follow and mute your followers who tweets within the last 3 months."
+    def follow_followers_with_mute
+      _follow_followers(true)
     end
 
     desc "unfollow_friends", "unfollow your friends who don't tweets within the last 3 months."
@@ -27,6 +22,53 @@ module Macaloni
         user = t.user(follower.id)
         begin
           t.unfollow(follower.id) unless active(user)
+        rescue Twitter::Error::TooManyRequests
+          raise
+        rescue Twitter::Error::Unauthorized
+          p 'this account might be private.'
+        end
+      end
+    end
+
+    desc "mute_friends", "mute friends who follows me."
+    def mute_friends
+      t.friends.each do |follower|
+        user = t.user(follower.id)
+        begin
+          t.mute(follower.id) if t.friendship?(follower, t.current_user)
+        rescue Twitter::Error::TooManyRequests
+          raise
+        rescue Twitter::Error::Unauthorized
+          p 'this account might be private.'
+        end
+      end      
+    end
+
+    desc "unmute_friends", "mute friends who doesn't follows me."
+    def unmute_friends
+      t.friends.each do |follower|
+        user = t.user(follower.id)
+        begin
+          t.unmute(follower.id) if t.friendship?(follower, t.current_user) == false and follower.muting? == true
+        rescue Twitter::Error::TooManyRequests
+          raise
+        rescue Twitter::Error::Unauthorized
+          p 'this account might be private.'
+        end
+      end      
+    end
+    
+    private
+
+    def _follow_followers(mute)
+      t.followers.each do |follower|
+        user = t.user(follower.id)
+        next if user.following?
+        begin
+          if active(user)
+            t.follow(follower.id)
+            t.mute(follower.id) if mute == true
+          end
         rescue Twitter::Error::TooManyRequests
           raise
         rescue Twitter::Error::Unauthorized
